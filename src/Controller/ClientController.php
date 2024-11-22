1c55327e68d374db51fe255679f66951ae2d0b5f
Martin <maballea.ledantec@gmail.com>
2024-11-22 10:18:09 +0100

    test

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -1,60 +1,18 @@
 <?php
 
-// src/Controller/ClientController.php
 namespace App\Controller;
 
-use App\Entity\Client;
-use App\Form\RegistrationFormType;
-use App\Security\LoginFormAuthenticator;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class ClientController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function home(Security $security): Response
+    #[Route('/acceuil', name: 'app_client')]
+    public function index(): Response
     {
-        $client = $security->getUser();
-        return $this->render('client/home.html.twig', [
-            'client' => $client
+        return $this->render('client/acceuil.html.twig', [
+            'controller_name' => 'ClientController',
         ]);
-    }
-
-    #[Route('/inscription', name: 'app_register')]
-    public function register(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $passwordEncoder): Response
-    {
-        $client = new Client();
-        $form = $this->createForm(RegistrationFormType::class, $client);
-        
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $client->setMotDePasse($passwordEncoder->encodePassword($client, $client->getMotDePasse()));
-            $em->persist($client);
-            $em->flush();
-
-            return $this->redirectToRoute('app_login'); // Redirige vers la page de connexion après l'inscription
-        }
-
-        return $this->render('client/register.html.twig', [
-            'registrationForm' => $form->createView(),
-        ]);
-    }
-
-    #[Route('/connexion', name: 'app_login')]
-    public function login(): Response
-    {
-        return $this->render('client/login.html.twig');
-    }
-
-    #[Route('/deconnexion', name: 'app_logout')]
-    public function logout(): void
-    {
-        // Symfony gère la déconnexion automatiquement
     }
 }
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,31 @@
+<?php
+
+// src/Form/RegistrationFormType.php
+namespace App\Form;
+
+use App\Entity\Client;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('email', TextType::class)
+            ->add('motDePasse', PasswordType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Client::class,
+        ]);
+    }
+}
+
